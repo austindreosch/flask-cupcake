59cae92e6411b8792fe13d697a45372d9734a3da
Austin Dreosch <austindreosch@gmail.com>
2022-10-22 22:45:29 -0500

    gdfgd

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ def connect_db(app):
 
 
 class Cupcake(db.Model):
-    __tablename__ = 'cupcake'
+    __tablename__ = 'cupcakes'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flavor = db.Column(db.Text, nullable=False)
@@ -18,3 +18,16 @@ class Cupcake(db.Model):
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False,
                       default="https://tinyurl.com/demo-cupcake")
+
+    def serialize(self):
+        """Returns a dict representation of todo which we can turn into JSON"""
+        return {
+            'id': self.id,
+            'flavor': self.flavor,
+            'size': self.size,
+            'rating': self.rating,
+            'image': self.image
+        }
+
+    def __repr__(self):
+        return f"<Cupcake {self.id} flavor={self.flavor} size={self.size} rating={self.rating} image={self.image}>"
